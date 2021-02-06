bedb288e0b4a4176dd35ca5dad8ebda999835b25
李洪健 <lihongjian@izaodao.com>
2021-02-06 16:35:51 +0800

    es

---
+++ lhj-service/src/main/java/com/lhj/controller/es/IndexController.java
@@ -0,0 +1,44 @@
+package com.lhj.controller.es;
+
+import com.lhj.service.es.IndexService;
+import com.lhj.utils.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The type Index controller.
+ *
+ * @author Zero
+ */
+@RestController
+@RequiredArgsConstructor
+public class IndexController {
+    private final IndexService indexService;
+
+    /**
+     * Gets single mappings.
+     *
+     * @param index the index
+     * @return the single mappings
+     */
+    @GetMapping("/{index}/mapping")
+    public Response<?> getSingleMappings(@PathVariable String index) {
+        return Response.of(indexService.getSingleMappings(index));
+    }
+
+    /**
+     * Clone index without data.
+     *
+     * @param sourceIndex the source index
+     * @param targetIndex the target index
+     * @return the response
+     */
+    @PostMapping("/{sourceIndex}/{targetIndex}/cloneIndexWithoutData")
+    public Response<?> cloneIndexWithoutData(@PathVariable String sourceIndex, @PathVariable String targetIndex) {
+        return Response.of(indexService.cloneIndexWithoutData(sourceIndex, targetIndex));
+    }
+
+}
